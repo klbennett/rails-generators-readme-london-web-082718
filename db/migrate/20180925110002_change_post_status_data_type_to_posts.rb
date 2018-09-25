404596aa6677a59753de0e78c4be4d7426df0280
klbennett <https://github.com/settings/applications>
2018-09-25 12:21:05 +0100

    Done.

--- db/migrate/20180925110002_change_post_status_data_type_to_posts.rb
+++ db/migrate/20180925110002_change_post_status_data_type_to_posts.rb
@@ -1,5 +1,5 @@
 class ChangePostStatusDataTypeToPosts < ActiveRecord::Migration
   def change
-    hange_column :posts, :post_status, :string
+    change_column :posts, :post_status, :string
   end
 end
